f4a1cf81dedb4162cd2c6ee96b64231ce2ceca94
MarinaRH <l_marina1717@hotmail.com>
2018-02-27 16:41:13 -0500

    agregando imagen ramdom

--- js/app.js
+++ js/app.js
@@ -14,6 +14,33 @@ AOS.init({
   duration: 2000
 });
 
+  //  Array con las imagenes y enlaces que se iran mostrando en la web  
+  var imagenes=new Array(
+    ['assets/images/mar.jpg'],
+    ['assets/images/marina.jpeg'],
+    ['assets/images/mar3.jpg']
+  );
+
+  // funcion para cabiar la iamgen
+  function rotarImagenes()
+  {
+    // obtenemos un numero aleatorio entre 0 y la cantidad de imagenes que hay
+    var index=Math.floor((Math.random()*imagenes.length));
+
+    // cambiamos la imagen
+    document.getElementById("imagen").src=imagenes[index][0];
+    // document.getElementById("link").href=imagenes[index][1];
+  }
+
+  onload=function()
+  {
+    // Cargamos una imagen aleatoria
+    rotarImagenes();
+
+    // Indicamos que cada 5 segundos cambie la imagen
+    setInterval(rotarImagenes,800);
+  }
+
 // Mostrar links de proyectos
 // $('.portafolio').hover(function(){
 //     $(this).removeClass('invisible');
