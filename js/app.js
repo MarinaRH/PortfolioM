2694eebbe26c079b90d709606b0bd2ed5af03bdd
MarinaRH <l_marina1717@hotmail.com>
2018-04-05 17:41:44 -0500

    Modificando ortografia

--- js/app.js
+++ js/app.js
@@ -14,38 +14,3 @@ AOS.init({
   duration: 2000
 });
 
-  //  Array con las imagenes y enlaces que se iran mostrando en la web  
-  var imagenes=new Array(
-    // ['assets/images/marina.webp'],
-    ['assets/images/mar3.webp']
-  );
-  // funcion para cabiar la iamgen
-  function rotarImagenes()
-  {
-    // obtenemos un numero aleatorio entre 0 y la cantidad de imagenes que hay
-    var index=Math.floor((Math.random()*imagenes.length));
-
-    // cambiamos la imagen
-    document.getElementById("imagen").src=imagenes[index][0];
-    // document.getElementById("link").href=imagenes[index][1];
-  }
-
-  onload=function()
-  {
-    // Cargamos una imagen aleatoria
-    rotarImagenes();
-
-    // Indicamos que cada 5 segundos cambie la imagen
-    setInterval(rotarImagenes,600);
-  }
-
-// Mostrar links de proyectos
-// $('.portafolio').hover(function(){
-//     $(this).removeClass('invisible');
-//     $(this).toggleClass('port-hov');
-//     $(this).toggleClass('boton-link');
-    // $('this'>'.im').toggleClass('port-hov');
-  
-  
-//   }
-// );
